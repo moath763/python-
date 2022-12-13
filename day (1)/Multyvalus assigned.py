5894955e88b3653c727da0ee6fb26ed37b62ac62
moath763 <m-m-s-02@hotmail.com>
2022-12-14 02:20:10 +0300

    Day 1  hours 1

---
+++ day (1)/Multyvalus assigned.py
@@ -0,0 +1,21 @@
+#lecture 3
+
+x,y,z=1,2,"moath"
+print(x)
+print(y)
+print(z)
+x=y=z=1
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+nums= [1,2,3]
+X,Y,Z=nums
+print(X)
+print(Y)
+print(Z)
---
+++ day (1)/syntax.py
@@ -0,0 +1,18 @@
+# lecture 1
+
+# comment used with the # sympol
+
+"""
+x=2
+
+if x > 1:
+  print('ahmed')
+
+if 6>2:
+  print('khaled')
+if 1>2:
+  print('khaled1')
+  print('khaled2')
+"""
+
+#A variable is created the moment you first assign a value to it.
