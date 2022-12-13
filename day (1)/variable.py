076da8ecf6ae9c4f1081d7bd354835f3524834be
moath763 <m-m-s-02@hotmail.com>
2022-12-14 02:24:52 +0300

    day1

---
+++ day (1)/variable.py
@@ -0,0 +1,26 @@
+#lecture 2
+x=2
+z=5
+print(x) # 2
+x=3
+y="ahmed"
+h="zyad"
+print(y)
+y ="moath"
+print(x) # 3
+print(y)
+print(x,y)
+print(x+z)
+print(y+h)
+
+#----------------------------------------------
+
+X = str(3)    # x will be '3'
+Y = int(3)    # y will be 3
+Z= float(3)  # z will be 3.0 
+print(X)
+print(Y)
+print(Z)
+
+print(type(X))
+print(type(Y)) 
